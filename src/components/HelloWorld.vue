47388b058148691177d3c7852cb7f9fd202055eb
akashi-iacsol <yuusuke_akashi@iacsol.co.jp>
2021-06-15 11:43:15 +0900

    akashi

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>{{ people }}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      people: [],
+      params: "",
+      samAccountName: "yuusuke_akashi",
+      password: "p@ssw0rd"
+    };
+  },
+  methods: {
+    postPerson() {
+      const path = "http://localhost:8080/api/auth";
+      this.params = new URLSearchParams();
+      this.params.append("samAccountName", this.samAccountName);
+      this.params.append("password", this.password);
+      axios
+        .post(path, this.params)
+        .then((response) => {
+          this.people = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.postPerson();
+  },
+};
+</script>
