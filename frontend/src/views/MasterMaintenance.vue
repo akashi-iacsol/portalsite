88d3254824b570b4cebb493fcae84d699d031a61
森中秀 <shu_morinaka@iacsol.co.jp>
2021-06-16 19:17:18 +0900

    ボタンに名称付与

--- frontend/src/views/MasterMaintenance.vue
+++ frontend/src/views/MasterMaintenance.vue
@@ -10,27 +10,27 @@
             <div class="box-body">
               <div class="row space-top">
                 <div class="col-md-4 col-md-offset-1">
-                  <button type="button" class="btn btn-block btn-primary btn-lg">Primary</button>
+                  <button type="button" class="btn btn-block btn-primary btn-lg">社員マスタ</button>
                 </div>
                 <div class="col-md-1"/>
                 <div class="col-md-4 col-md-offset-1">
-                  <button type="button" class="btn btn-block btn-success btn-lg">Success</button>
+                  <button type="button" class="btn btn-block btn-success btn-lg">部署マスタ</button>
                 </div>
                 <div class="col-md-1"/>
               </div>
               <div class="row space-top">
                 <div class="col-md-4 col-md-offset-1">
-                  <button type="button" class="btn btn-block btn-primary btn-lg">Primary</button>
+                  <button type="button" class="btn btn-block btn-primary btn-lg">役職マスタ</button>
                 </div>
                 <div class="col-md-7"/>
               </div>
               <div class="row space-top space-bottom">
                 <div class="col-md-4 col-md-offset-1">
-                  <button type="button" class="btn btn-block btn-primary btn-lg">Primary</button>
+                  <button type="button" class="btn btn-block btn-primary btn-lg">所属管理マスタ</button>
                 </div>
                 <div class="col-md-1"/>
                 <div class="col-md-4 col-md-offset-1">
-                  <button type="button" class="btn btn-block btn-success btn-lg">Success</button>
+                  <button type="button" class="btn btn-block btn-success btn-lg">権限管理マスタ</button>
                 </div>
                 <div class="col-md-1"/>
               </div>
