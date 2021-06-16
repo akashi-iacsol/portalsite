bf5a391c1c2f47e03be2ea5254a6f668862716d9
akashi-iacsol <yuusuke_akashi@iacsol.co.jp>
2021-06-16 17:18:48 +0900

    App.vueにヘッダーの設定とページタイトルの設定

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Header v-if="path !== '/login'" />
+    <router-view />
   </div>
 </template>
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  data() {
+    return {
+      path: "",
+    };
+  },
+  components: {
+    Header,
+  },
+  methods: {
+    createPageTitle(to) {
+      if (to.path === "/login") {
+        document.title = "ログイン";
+      } else if (to.meta.title) {
+        var setTitle = to.meta.title + " | IACSOLポータルサイト";
+        document.title = setTitle;
+      } else {
+        document.title = "IACSOLポータルサイト";
+      }
+    },
+  },
+  mounted() {
+    var to = this.$route;
+    this.path = to.path;
+    this.createPageTitle(to);
+  },
+  watch: {
+    $route(to) {
+      this.path = to.path;
+      this.createPageTitle(to);
+    },
+  },
+};
+</script>
