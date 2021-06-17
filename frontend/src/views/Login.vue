0c794709d409ff665681c72f0482c374d6a5ced2
akashi-iacsol <yuusuke_akashi@iacsol.co.jp>
2021-06-17 14:35:31 +0900

    css等修正。

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="login">
-    <div class="header">
-      <img alt="iacsol logo" src="../assets/logo.png" class="logo" />
+    <div class="login-header login-div">
+      <img alt="iacsol logo" src="../assets/logo.png" class="login-logo" />
     </div>
     <div class="container">
-      <div class="box">
-        <div>
+      <div class="login-box">
+        <div class="login-div">
           <h3 colspan="2">ログイン</h3>
         </div>
 
-        <div class="login-error">
-          <a v-if="isError">ログインできませんでした。</a>
+        <div class="login-div">
+          <a class="login-error" v-if="isError">ログインできませんでした。</a>
           <br v-if="!isError" />
         </div>
 
-        <table class="form">
-          <tr>
+        <table class="login-form">
+          <tr class="login-tr">
             <td>ユーザ名:</td>
             <td>
               <input v-model="userID" @keyup.enter="login" />
             </td>
           </tr>
-          <tr>
+          <tr class="login-tr">
             <td>パスワード:</td>
             <td>
               <input v-model="password" type="password" @keyup.enter="login" />
@@ -30,7 +30,7 @@
         </table>
 
         <br />
-        <div>
+        <div class="login-div">
           <button @click="login">ログイン</button>
         </div>
         <br />
@@ -74,7 +74,7 @@ export default {
             attributeArray = res.data.find((item) => item[0] === "OU");
             this.userOU = attributeArray.slice(1, attributeArray.length);
             // this.getDB();
-            this.$router.push("/");
+            this.$router.push("/portal");
           } else {
             this.userName = "";
             this.userOU = [];
@@ -132,35 +132,26 @@ export default {
 </script>
 
 <style>
-div {
+.login-div {
   text-align: center;
 }
-table {
+.login-form {
   margin-left: auto;
   margin-right: auto;
 }
-tr {
+.login-tr {
   height: 3em;
 }
-.header {
+.login-header {
   text-align: left;
 }
-.logo {
+.login-logo {
   width: 50px;
   height: 50px;
 }
-.container {
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-}
-.box {
+.login-box {
   display: inline-block;
+  background-color: ghostwhite;
   padding: 0 3em;
   border: solid 3px steelblue;
   border-radius: 10px;
