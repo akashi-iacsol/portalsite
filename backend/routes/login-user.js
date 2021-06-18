5045557bbf4560c4ad24547d12a390b9325bce9b
akashi-iacsol <yuusuke_akashi@iacsol.co.jp>
2021-06-18 11:39:15 +0900

    then catch に変更

--- backend/routes/login-user.js
+++ backend/routes/login-user.js
@@ -8,42 +8,39 @@ let employee_first_name = "";
 let department = [];
 let authority_code = [];
 
-/* GET users listing. */
 router.post('/', function (req, res) {
-  pool.connect(function (err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      client.query(`SELECT e.employee_number, e.employee_last_name, e.employee_first_name FROM employee_master e  WHERE e.employee_id = '${req.body.employee_id.replace(/\"/g, "\'")}';`, function (err, result) {
-        if (err) {
-          console.log(err);
-        } else {
+  pool.connect().then((client) => {
+      client.query(`SELECT e.employee_number, e.employee_last_name, e.employee_first_name FROM employee_master e  WHERE e.employee_id = '${req.body.employee_id.replace(/\"/g, "\'")}';`)
+        .then((result) => {
           employee_number = String(result.rows[0].employee_number);
           employee_last_name = String(result.rows[0].employee_last_name);
           employee_first_name = String(result.rows[0].employee_first_name);
 
-          client.query(`SELECT d.department_code, d.department_name FROM department_management dm LEFT JOIN department_master d ON dm.department_code = d.department_code WHERE dm.employee_number = '${employee_number.replace(/\"/g, "\'")}';`, function (err, result) {
-            if (err) {
-              console.log(err);
-            } else {
+          client.query(`SELECT d.department_code, d.department_name FROM department_management dm LEFT JOIN department_master d ON dm.department_code = d.department_code WHERE dm.employee_number = '${employee_number.replace(/\"/g, "\'")}';`)
+            .then((result) => {
               department = result.rows;
-              client.query(`SELECT a.authority_code FROM authority_management a WHERE a.employee_number = '${employee_number.replace(/\"/g, "\'")}';`, function (err, result) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  authority_code = []
+
+              client.query(`SELECT a.authority_code FROM authority_management a WHERE a.employee_number = '${employee_number.replace(/\"/g, "\'")}';`)
+                .then((result) => {
                   authority_code = result.rows;
-                  console.log(authority_code)
                   res = sendJson(res);
-                }
-              });
-            }
-          });
-        }
-      });
-    }
+                })
+                .catch((error) => {
+                  console.log(error)
+                });
+            })
+            .catch((error) => {
+              console.log(error)
+            });
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+  }).catch((error) => {
+    console.log(error)
   });
 });
+
 function sendJson(res) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
   res.json({
